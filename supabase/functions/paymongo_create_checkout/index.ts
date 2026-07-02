dea9e7eb8536dfbd91177ae085dc3e5b109efcd5
LinkMe-project-website <johnlloydbiendimas@gmail.com>
2026-07-02 14:19:04 +0800

    Change environment variable for Supabase URL

--- supabase/functions/paymongo_create_checkout/index.ts
+++ supabase/functions/paymongo_create_checkout/index.ts
@@ -1,7 +1,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const PAYMONGO_SECRET_KEY = Deno.env.get("PAYMONGO_SECRET_KEY")!;
-const SUPABASE_URL = Deno.env.get("SUPABASE_URL")!;
+const SUPABASE_URL = Deno.env.get("SB_SERVICE_ROLE_KEY")!;
 const SUPABASE_SERVICE_ROLE_KEY = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!;
 
 // TODO: palitan mo ito ng totoong presyo mo sa pesos (in centavos, so 200.00 = 20000)
